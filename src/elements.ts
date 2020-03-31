70bfecedb243b23a9e198980a45490b4006b9945
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2020-03-31 17:46:38 +0900

    revert undefined fix

--- src/elements.ts
+++ src/elements.ts
@@ -75,7 +75,7 @@ export class ElementsError extends Error {
 }
 
 const ParseResult = <IType>(val: IType | Error | { code: number; message: string; }): IType => {
-    if (typeof val === "string" || typeof val === "number" || typeof val === "boolean" || typeof val === "undefined") {
+    if (typeof val === "string" || typeof val === "number" || typeof val === "boolean") {
         return val;
     } else if ("code" in val) {
         throw new ElementsError(val.code, val.message);
